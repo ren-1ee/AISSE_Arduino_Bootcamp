f9d305adb8339b0e235bd54b4765faecec67f603
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2023-07-24 21:53:42 +1000

    add some vs stuff and fix up code.

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -2,23 +2,24 @@
   Author: Crawford Lieu @DefNotCrawf
   Learning Intention: The students will understand the difference between the inbuilt setup and loop methods, and understand the basics of the serial monitor'.
   Success Criteria:
-    1. I understand the different behaviours of the setup and loop methods
-    2. I can print information to the serial monitor
-    3. I can print information to a new line of the serial monitor
-    4. I can access the serial plotter to see data output over time
-    5. I understand the impact of having more than one setup or loop methods in my sketch
+	1. I understand the different behaviours of the setup and loop methods
+	2. I can print information to the serial monitor
+	3. I can print information to a new line of the serial monitor
+	4. I can access the serial plotter to see data output over time
+	5. I understand the impact of having more than one setup or loop methods in my sketch
   
   Student Notes: 
+	'Serial.println' will print text to a new line in serial monitor
 
   Documentation: 
-    https://www.arduino.cc/reference/en/language/functions/communication/serial/
-    https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
+	https://www.arduino.cc/reference/en/language/functions/communication/serial/
+	https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
 */
 
 // The setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello World!");
+  Serial.print("Hello World!");
 }
 
 // The loop function runs over and over again forever
