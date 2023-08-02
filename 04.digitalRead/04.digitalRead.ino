9a6cadba96e3ab334906ee9dcf59b85d20786e3d
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2023-08-02 15:03:25 +1000

    (W.I.P.) Update 04.digitalRead.ino

--- 04.digitalRead/04.digitalRead.ino
+++ 04.digitalRead/04.digitalRead.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Crawford Lieu
   Learning Intention: The students will learn how to wire a pull-up and pull-down switch and configure PINs for digital input and read binary data from those PINs.
   Success Criteria:
     1. I understand how to configure a PIN for input
@@ -7,7 +7,7 @@
     3. I know how to read binary data from a specific PIN
     4. I understand that LOW = false = 0 = <1.5 volts (or Off) and that HIGH = true = 1 = >3 volts (or On)
     5. I know the difference between a switch and a momentary switch
-    6. I can organise output data so it correctly plots on the serial monitor
+    6. I can organise input data so it correctly plots on the serial monitor
     7. I can apply this knowledge to the Button and the Line Finder in the sensor kit
 
   Student Notes: 
@@ -20,10 +20,29 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/04.digitalRead/Bootcamp-digitalRead.png
 */
 
-void setup() {
+static unsigned int myLED = 4;
+static unsigned int myBuzzer = 3;
+static unsigned int myButton = 2;
 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial monitor configured to 9600br.");
+  Serial.println("------------------------------------------------------");
+  pinMode(myLED, OUTPUT);
+  pinMode(myBuzzer, OUTPUT);
+  pinMode(myButton, INPUT);
 }
 
 void loop() {
+  int val = digitalRead(myButton);
+  Serial.println(String(val));
+}
 
+void myAlert() {
+  digitalWrite(myLED, HIGH);
+  tone(myBuzzer, 128);
+  delay(500);
+  digitalWrite(myLED, LOW);
+  noTone(myBuzzer);
+  delay(5000);
 }
