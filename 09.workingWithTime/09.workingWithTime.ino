020ebc8666f24eb5b8d719cfe96a92faaf3c5dfe
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2023-08-22 10:16:21 +1000

    Complete Activity 9

--- 09.workingWithTime/09.workingWithTime.ino
+++ 09.workingWithTime/09.workingWithTime.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Crawford Lieu
    Learning Intention: The students understand how time is used in microcontrollers and best practices
     1. I understand why delay(1000) whilst easy is not the preferred way of time event management
     2. I can use the millis() method and binary logic to trigger events by time
@@ -14,11 +14,33 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/09.workingWithTime/Bootcamp-workingWithTime.png
 */
 
+// Set-up LED and bool
+static unsigned int myLED = 13;
+bool myLEDStatus = false;
+
+// Both as longs
+unsigned long myLEDPrevMillis = 0;
+const unsigned long myLEDInterval = 2000;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("---");
+  Serial.println("SM configured to @9600.");
+  Serial.println("--------------------------------");
 
+  // Configure LED
+  pinMode(myLED, OUTPUT);
 }
 
 void loop() {
- 
+  unsigned long currentMillis = millis();
+  digitalWrite(myLED, myLEDStatus);
+  Serial.print(currentMillis);
+  Serial.print(",");
+  Serial.println(myLEDStatus);
+
+  if (currentMillis - myLEDPrevMillis >= myLEDInterval) {
+    myLEDStatus = !myLEDStatus;
+    myLEDPrevMillis = currentMillis;
+  }
 }
